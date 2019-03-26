d523a331bd260fb37df1ecd76b354d61281cb330
Jakub Bielawa <jakub.bielawa@gmail.com>
2019-03-26 01:00:58 +0000

    Added two more loops

--- ConfigParser/ConfigParser/main.cpp
+++ ConfigParser/ConfigParser/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <algorithm>
 
 #include "cfgparser.h"
 
@@ -11,12 +12,29 @@ int main()
 	map<string, string> config;
 	parseconfig("sample.ini", &config);
 
+	// STL iterator
 	map<string, string>::iterator it = config.begin();
 	while (it != config.end())
 	{
 		cout << "[" << it->first << "] = " << it->second << endl;
 		it++;
 	}
+
+	cout << endl;
+
+	// range based FOR loop
+	for (pair<string, string> entry : config)
+	{
+		cout << "[" << entry.first << "] = " << entry.second << endl;
+	}
+
+	cout << endl;
+
+	// for_each and a simple lambda function
+	for_each(config.begin(), config.end(), 
+		[](pair<string, string> entry) {
+			cout << "[" << entry.first << "] = " << entry.second << endl;
+	});
 
 	cin.get();
 }
